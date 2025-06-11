65949f3267bbf66f101782f77eb37d221e4931fd
ASMILED <lopezbeto519@gmail.com>
2025-06-10 21:52:06 -0700

    Add files via upload

---
+++ happy_bd/script.js
@@ -0,0 +1,5 @@
+function celebrar() {
+    //alert("¡Las cosas no siempre son lo que parecen. Espera hasta que mis semillas echen raíces!");
+
+    window.location.href = "felicidades.html"; // Navigates to the new URL
+}
